f0c3eb38931195f39c3bb7f5b4e3a5a3128111e8
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-03-02 00:08:59 +0600

    error handling

--- js/phone.js
+++ js/phone.js
@@ -1,18 +1,37 @@
+document.getElementById('error-message').style.display = 'none';
+
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // clear data
     searchField.value = '';
-
-    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displaySearchResult(data.data))
+    if (searchText == '') {
+        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('error-message').innerText = 'Please Write Something to search Phone';
+    }
+    else {
+        // load data 
+        const url = ` https://openapi.programming-herooo.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => displaySearchResult(data.data))
+            .catch(error => displayError(error));
+    }
+    
 }
+const displayError = () => {
+    document.getElementById('error-message').style.display = 'block';
+    document.getElementById('error-message').innerText = 'Something went wrong. Please Try again later';
+}
+
 
 const displaySearchResult = phones => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
+    if (phones.length == 0) {
+        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('error-message').innerText = 'No Result Found';
+    }
     phones.forEach(phone => {
         // console.log(phone)
         const div = document.createElement('div');
@@ -28,7 +47,7 @@ const displaySearchResult = phones => {
             </div>
             `;
         searchResult.appendChild(div);
-        
+
     })
 }
 
@@ -37,11 +56,12 @@ const loadPhoneDetail = id => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetail(data.data));
-    
+
 }
 const displayPhoneDetail = phone => {
-    console.log(phone);
+    // console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent='';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
