e5cc4dea1e275c416db781f9f15b95c67a4935bb
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-03-01 16:13:14 +0600

    cards added using js

--- js/phone.js
+++ js/phone.js
@@ -7,8 +7,26 @@ const searchPhone = () => {
     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => displaySearchResult(data.data))
+}
 
-    console.log(searchText);
-    console.log(url);
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = ''
+    phones.forEach(phone => {
+        console.log(phone)
+
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+            `;
+            searchResult.appendChild(div);
+    })
 }
